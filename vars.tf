ecdca27c5c9bbf3bb317ff605535086a51311066
noyon <mohaymenul.islam@m2amedia.tv>
2022-04-18 20:10:00 +0600

    variables added

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "IncomingWebHookURL" {
+  type = string
+  description = "Incoming Webhook URL for slack app. To create follow this [Doc](https://api.slack.com/messaging/webhooks#getting_started)"
+}
+
+variable "SlackChannel" {
+  type = string
+  description = "Slack Chennel Name"
+  default = "security_alerts"
+}
